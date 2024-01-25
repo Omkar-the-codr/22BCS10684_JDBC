964bace31126605793705e0d7d41ec6d54145d34
Omkar the codr <omkarchaturvedi2004@gmail.com>
2024-01-25 14:39:26 +0530

    Commit

--- jdbc/src/com/capgemini/Main/MainClass.java
+++ jdbc/src/com/capgemini/Main/MainClass.java
@@ -1,9 +1,17 @@
 package com.capgemini.Main;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class MainClass {
 
-	public static void main(String[] args) {
-		System.out.println("HEllo World");
+	public static void main(String[] args) throws ClassNotFoundException, SQLException
+	{
+		Connection con = null;
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		con = DriverManager.getConnection("jdbc:mysql://localhost:3306/cuims","rmootoo","Yourpasword");
+		System.out.println(con.getClass().getName());
 
 	}
 
